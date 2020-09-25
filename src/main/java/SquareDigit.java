7309190610835ff28e6c1c31a683d7f2de57dc46
marco.romanutti <marco.romanutti@intersys.ch>
2020-09-25 15:02:05 +0200

    Kata refactored

--- src/main/java/SquareDigit.java
+++ src/main/java/SquareDigit.java
@@ -1,43 +1,15 @@
-import java.util.LinkedList;
-
 public class SquareDigit {
 
     public int squareDigits(int n) {
-        int result = 0;
+        String result = "";
 
-        LinkedList<Integer> stack = new LinkedList<Integer>();
-
-        int digit, square, m;
-        while (n > 0) {
-            // Calculate current digit and its square
-            digit = n % 10;
-            square = digit == 0 ? 0 : digit * digit;
-            stack.push(square);
-
-            // Prepare next iteration
-            n /= 10;
+        while (n != 0) {
+            int digit = n % 10 ;
+            result = digit*digit + result ;
+            n /= 10 ;
         }
 
-        // Reverse order
-        while (!stack.isEmpty()) {
-            square = stack.pop();
-
-            m = square;
-            if (m == 0) {
-                // Handle 0
-                result = result * 10;
-            } else {
-                // Shift result to left
-                while (m > 0) {
-                    result = result * 10;
-                    m /= 10;
-                }
-            }
-
-            // Append to result
-            result += square;
-        }
-
-        return result;
+        return Integer.parseInt(result) ;
     }
+
 }
